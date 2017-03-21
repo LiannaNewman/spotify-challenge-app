b0c5404078ee1a108c2d98223fec848b9f78f473
Lianna Newman <liannanewman@gmail.com>
2017-03-21 03:53:38 -0400

    final updates

--- app/assets/javascripts/controllers/person_ctrl.js
+++ app/assets/javascripts/controllers/person_ctrl.js
@@ -13,6 +13,9 @@
     $scope.addPerson = function(person) {
       $http.post('/api/v1/people.json', person).then(function(response) {
         $scope.people.push(person);
+        $http.get('/api/v1/people.json').then(function(response) {
+          $scope.people = response.data;
+        });
       });
       $scope.person = {};
     };
@@ -23,7 +26,12 @@
         name: person.name,
         favoriteCity: person.favoriteCity
       };
-      $http.patch('/api/v1/people/' + person.id + '.json', personParams).success(success).error(error);
+      $http.put('/api/v1/people/' + person.id + '.json', personParams).then(function(response) {
+        if(response.data)
+          $scope.msg = 'Person was Successfully Updated';
+        }, function(response) {
+          $scope.msg = 'Something went wrong, please try again.';
+        });
     };
 
     $scope.deletePerson = function(person) {
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,5 @@
 class PeopleController < ApplicationController
+  skip_before_filter :verify_authenticity_token
 
   def index
     @people = Person.all
